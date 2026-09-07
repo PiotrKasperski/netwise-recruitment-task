c16bebd77885abe4c543c85d16cbf77a06467aa9
Piotr Kasperski <piotr.kasperski94@gmail.com>
2026-09-07 19:51:20 +0200

    endpoint repaired (#13)

--- NetwiseRecruitmentTask/ServiceConfiguration.cs
+++ NetwiseRecruitmentTask/ServiceConfiguration.cs
@@ -7,7 +7,7 @@ public static class ServiceConfiguration
         services.AddHostedService<Worker>();
         services.AddHttpClient<ICatFactApiService, CatFactApiService>(client =>
         {
-            client.BaseAddress = new Uri("https://catfact.ninja/fact/");
+            client.BaseAddress = new Uri("https://catfact.ninja/");
             client.Timeout = TimeSpan.FromSeconds(10);
         });
         services.AddSingleton<IFilesystemService, FilesystemService>();
