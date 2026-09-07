77056468ff7fa1bce905e438461d6c7f06ac88ed
klonek <piotr.kasperski94@gmail.com>
2026-09-07 23:21:19 +0200

    Add data annotation to CatFactSettings

--- NetwiseRecruitmentTask/CatFactSettings.cs
+++ NetwiseRecruitmentTask/CatFactSettings.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 public sealed class CatFactSettings
 {
     public const string SectionName = "CatFactSettings";
 
+    [Required]
+    [Url]
     public string BaseAddress { get; set; } = string.Empty;
+
+    [Required]
     public string FactEndpoint { get; set; } = string.Empty;
+
+    [Range(1, 300)]
     public int RequestTimeoutSeconds { get; set; } = 10;
+
+    [Required]
     public string OutputFileName { get; set; } = "cat_facts.txt";
 }
