dd06da89b52772cb184ae7ff977e37ca1ab61ac0
Piotr Kasperski <piotr.kasperski94@gmail.com>
2026-09-07 16:54:06 +0200

    menu added (#6)

--- NetwiseRecruitmentTask/Worker.cs
+++ NetwiseRecruitmentTask/Worker.cs
@@ -1,13 +1,38 @@
 using Microsoft.Extensions.Hosting;
 
-public sealed class Worker(ICatFactApiService _catFactApiService, IFilesystemService _filesystemService) : BackgroundService
+public sealed class Worker(IHostApplicationLifetime hostApplicationLifetime, ICatFactApiService _catFactApiService, IFilesystemService _filesystemService) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _filesystemService.EnsureFileExist();
-        var fact = await _catFactApiService.GetCatFactAsync();
-        await _filesystemService.AppendLineAsync("Fact: " + fact.Fact + " length: " + fact.Length.ToString());
         Console.Clear();
-        Console.WriteLine(fact.Fact);
+        _filesystemService.EnsureFileExist();
+        while (true)
+        {
+            Console.WriteLine("Push ENTER to get new fact or type 'exit' to close the app");
+
+            Console.Write("> ");
+            var input = Console.ReadLine();
+
+            if (String.Equals(input, "exit", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Exited");
+                break;
+            }
+
+            try
+            {
+                var fact = await _catFactApiService.GetCatFactAsync();
+                Console.Clear();
+                await _filesystemService.AppendLineAsync($"Fact: {fact.Fact} length: {fact.Length}");
+                Console.WriteLine(fact.Fact);
+            }
+            catch (Exception ex)
+            {
+                TextWriter errorWriter = Console.Error;
+                errorWriter.WriteLine(ex.Message);
+            }
+        }
+        hostApplicationLifetime.StopApplication();
+
     }
 }
