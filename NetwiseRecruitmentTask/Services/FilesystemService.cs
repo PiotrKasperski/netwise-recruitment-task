0474ac63f595fa0bd2e4aaed8a21b0fa976fdb58
klonek <piotr.kasperski94@gmail.com>
2026-09-07 15:37:16 +0200

    filesystem service created

---
+++ NetwiseRecruitmentTask/Services/FilesystemService.cs
@@ -0,0 +1,24 @@
+
+public sealed class FilesystemService : IFilesystemService
+{
+    public string FilePath { get; }
+    public FilesystemService()
+    {
+        FilePath = Path.Combine(AppContext.BaseDirectory, "cat_facts.txt");
+    }
+
+
+    public async Task AppendLineAsync(string line, CancellationToken cancellationToken = default)
+    {
+        await File.AppendAllTextAsync(FilePath, line + Environment.NewLine, cancellationToken);
+
+    }
+
+    public void EnsureFileExist()
+    {
+        if (!File.Exists(FilePath))
+        {
+            using var stream = File.Create(FilePath);
+        }
+    }
+}
