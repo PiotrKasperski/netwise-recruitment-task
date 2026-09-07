cb0a2718ceccfc3eb331473151783bde660d6552
klonek <piotr.kasperski94@gmail.com>
2026-09-07 15:01:46 +0200

    CatFactApiService

--- NetwiseRecruitmentTask/Program.cs
+++ NetwiseRecruitmentTask/Program.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
+builder.Services.AddHttpClient<ICatFactApiService, CatFactApiService>(client =>
+{
+    client.BaseAddress = new Uri("https://catfact.ninja/");
+    client.Timeout = TimeSpan.FromSeconds(10);
+});
