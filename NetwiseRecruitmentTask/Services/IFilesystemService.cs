bc735079c43c7992ab9410933906b2fb7559d31b
Piotr Kasperski <piotr.kasperski94@gmail.com>
2026-09-07 15:57:55 +0200

    3 create filesystem service (#4)
    * Added filesystem service interface
    
    * filesystem service created
    
    * add Json serialization

---
+++ NetwiseRecruitmentTask/Services/IFilesystemService.cs
@@ -0,0 +1,7 @@
+public interface IFilesystemService
+{
+    void EnsureFileExist();
+    Task AppendLineAsync(string line, CancellationToken cancellationToken = default);
+
+    string FilePath { get; }
+}
